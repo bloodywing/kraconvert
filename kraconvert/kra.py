a426796dccba5e7a34925430caf9fafde7845865
bloodywing <bloodywing@neocomy.net>
2015-09-29 18:44:05 +0200

    Path inside the .kra file is not always '.kra' basename

--- kraconvert/kra.py
+++ kraconvert/kra.py
@@ -6,6 +6,8 @@ from lxml import etree as ET
 
 __author__ = 'pierre'
 
+ns = {'kra': 'http://www.calligra.org/DTD/krita'}
+
 
 class Kra(object):
 
@@ -13,6 +15,7 @@ class Kra(object):
     merged_image = None
     basename = None
     icc = None
+    kra_name = None
 
     def __init__(self, krafile):
 
@@ -25,8 +28,12 @@ class Kra(object):
 
 
         self.merged_image = kra.read('mergedimage.png')
+
         self.xml = ET.fromstring(kra.read('maindoc.xml'))
-        self.icc = kra.read('{basename}/annotations/icc'.format(basename=self.basename))
+        self.kra_name = self.xml.find('.//kra:IMAGE', ns).attrib['name']
+
+        self.icc = kra.read('{basename}/annotations/icc'.format(basename=self.kra_name))
+
 
     def get_basename(self):
         return self.basename
@@ -35,7 +42,6 @@ class Kra(object):
         return self.merged_image
 
     def get_icc(self):
-        ns = {'kra': 'http://www.calligra.org/DTD/krita'}
         x = self.xml.find('.//kra:IMAGE', ns)
         icc_name = x.attrib['profile']
         return {'name': icc_name, 'data': self.icc}
