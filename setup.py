646c0794be9c224092e5d7bbdc64e6a380046558
bloodywing <bloodywing@neocomy.net>
2015-10-04 14:17:09 +0200

    Added missing long_description in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 import kraconvert
+import os
 
 packages = [
     'kraconvert',
@@ -23,6 +24,11 @@ requires = [
     'lxml', 'pillow'
 ]
 
+here = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(here, 'README.md'))
+long_description = f.read().strip()
+f.close()
+
 setup(
     name='kraconvert',
     version=kraconvert.__version__,
@@ -32,6 +38,8 @@ setup(
     package_data=package_data,
     entry_points=entry_points,
     install_requires=requires,
+    requires=requires,
+    long_description=long_description,
     author='Pierre Geier <bloodywing>',
     author_email='pierre@isartistic.biz',
     download_url='https://github.com/bloodywing/kraconvert/tarball/{tag}'.format(tag=kraconvert.__version__),
